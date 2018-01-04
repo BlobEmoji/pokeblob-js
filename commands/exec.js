27c904081cc5ab7ba56aded4c89c6c3cbc5550e5
OG Novuh <jamooney2000@gmail.com>
2018-01-04 16:36:59 -0500

    Update exec

--- commands/exec.js
+++ commands/exec.js
@@ -11,7 +11,7 @@ class Exec extends Command {
       category: 'System',
       extended: 'This will spawn a child process and execute the given command.',
       botPerms: ['SEND_MESSAGES'],
-      permLevel: 'Police'
+      permLevel: 'Bot Owner'
     });
   }
 
