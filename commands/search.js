758e77cae0e36221586cef96aeaa0e2a2b4da6c8
Tiemen <Tiemen2@live.nl>
2018-01-10 13:32:02 +0100

    Small fixes

--- commands/search.js
+++ commands/search.js
@@ -31,15 +31,15 @@ class Search extends Command {
     const msg = await message.channel.send(`_${message.author} searches through the tall grass and finds..._`);
     await this.client.wait(2500);    
 
-    var blobChance = 1/3;
-    var moneyChance = 1/3;            
+    const blobChance = 1/3;
+    const moneyChance = 1/3;            
 
-    var roll = Math.random();    
+    let roll = Math.random();    
     if (roll < blobChance) {
       //To-do add catch
-      var blobTier = '';
-      var tierSize = 0;
-      var blobName = '';
+      let blobTier = '';
+      let tierSize = 0;
+      let blobName = '';
       
       roll = Math.random();
       if (roll < commonChance) {
@@ -67,17 +67,17 @@ class Search extends Command {
         
       }
       const blob = this.client.emojis.find('name', blobName);
-      msg.edit(`_${message.author} searches through the tall grass and finds..._ ${blobTier} ${blob}**!** You have ${energy.points} energy remaining.\nType \`.catch\` to try and capture it!\n\`.search\` to let this blob run away and continue looking (1 energy)\n\`.cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
+      msg.edit(`_${message.author} searches through the tall grass and finds..._ ${blobTier} ${blob}**!** You have ${energy.points} energy remaining.\nType \`${this.client.config.prefix}catch\` to try and capture it!\n\`${this.client.config.prefix}search\` to let this blob run away and continue looking (1 energy)\n\`${this.client.config.prefix}cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
     }
     else if (roll >= blobChance && roll < blobChance + moneyChance) {
-      var money = Math.ceil(Math.random()*10);
+      const money = Math.ceil(Math.random()*10);
       const coins = await this.client.coins.get(`${message.guild.id}-${message.author.id}`);
       coins.coins += money;
       await this.client.coins.set(`${message.guild.id}-${message.author.id}`, coins);
-      msg.edit(`_${message.author} searches through the tall grass and finds..._ ${money}<:blobcoin:398579309276823562>**!** You have ${energy.points} energy remaining.\n\`.search\` continue looking (1 energy)\n\`.cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
+      msg.edit(`_${message.author} searches through the tall grass and finds..._ ${money} <:blobcoin:398579309276823562>**!** You have ${energy.points} energy remaining.\n\`${this.client.config.prefix}search\` continue looking (1 energy)\n\`${this.client.config.prefix}cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
     }
     else {
-      msg.edit(`_${message.author} searches through the tall grass and finds..._ nothing**!** You have ${energy.points} energy remaining.\n\`.search\` to continue looking (1 energy)\n\`.cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
+      msg.edit(`_${message.author} searches through the tall grass and finds..._ nothing**!** You have ${energy.points} energy remaining.\n\`${this.client.config.prefix}search\` to continue looking (1 energy)\n\`${this.client.config.prefix}cancel\` to let the blob run away and stop searching`); // eslint-disable-line no-undef
     }
 
 
