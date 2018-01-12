5cab5c9bb2028227ee16e1a6df48e5e7863a5bf6
Gorialis <sansgorialis@gmail.com>
2018-01-13 08:20:00 +0900

    Show blob count in user command

--- commands/user.js
+++ commands/user.js
@@ -16,21 +16,24 @@ class User extends Command {
   async run(message, args, level) { // eslint-disable-line no-unused-vars
     const target = (message.mentions.members.length > 0) ? message.mentions.members[0] : message.author;
     const connection = await this.client.db.acquire();
-    let userData, inventory;
+    let userData, inventory, blobData;
     try {
       userData = await this.client.db.getUserData(connection, message.guild.id, target.id);
       inventory = await this.client.db.getUserInventory(connection, message.guild.id, target.id);
+      blobData = await this.client.db.getUserBlobs(connection, message.guild.id, target.id);
     } finally {
       connection.release();
     }
     let invFormatting = inventory.map(x => `${x.amount}x ${x.name}`).join();
+    let blobCount = blobData.filter(x => x.caught && x.amount > 0).length;
+    let blobsSeen = blobData.length;
     if (invFormatting === '') invFormatting = 'Empty';
     const embed = new MessageEmbed()
-      .setAuthor(target.username, target.displayAvatarURL)
+      .setAuthor(target.username, target.displayAvatarURL())
       .setTimestamp()
       .addField('Member Energy', `${userData.energy}`, true)
       .addField('Inventory', `${invFormatting}`, true)
-      .addField('Total Blobs Caught', '<insert info here>', true)
+      .addField('Total Blobs Caught', `${blobCount} (${blobsSeen} seen)`, true)
       .setFooter('PokéBlobs');
     message.channel.send({ embed });
   }
