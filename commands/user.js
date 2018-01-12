7218cb34a0a17bb4499072020032e5143511e93e
Gorialis <sansgorialis@gmail.com>
2018-01-13 07:55:11 +0900

    Change user command to use working target

--- commands/user.js
+++ commands/user.js
@@ -14,19 +14,19 @@ class User extends Command {
   }
 
   async run(message, args, level) { // eslint-disable-line no-unused-vars
-    const target = args[0];
+    const target = (message.mentions.members.length > 0) ? message.mentions.members[0] : message.author;
     const connection = await this.client.db.acquire();
     let userData, inventory;
     try {
-      userData = await this.client.db.getUserData(connection, message.guild.id, target);
-      inventory = await this.client.db.getUserInventory(connection, message.guild.id, target);
+      userData = await this.client.db.getUserData(connection, message.guild.id, target.id);
+      inventory = await this.client.db.getUserInventory(connection, message.guild.id, target.id);
     } finally {
       connection.release();
     }
     let invFormatting = inventory.map(x => `${x.amount}x ${x.name}`).join();
     if (invFormatting === '') invFormatting = 'Empty';
     const embed = new MessageEmbed()
-      .setAuthor(message.author.username, message.author.displayAvatarURL)
+      .setAuthor(target.username, target.displayAvatarURL)
       .setTimestamp()
       .addField('Member Energy', `${userData.energy}`, true)
       .addField('Inventory', `${invFormatting}`, true)
