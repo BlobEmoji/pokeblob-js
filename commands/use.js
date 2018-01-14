d515f9335278a610b1d45ab88f55dc24270e678a
OG Novuh <jamooney2000@gmail.com>
2018-01-14 16:22:15 -0500

    Base for Use.

---
+++ commands/use.js
@@ -0,0 +1,27 @@
+const Command = require('../base/Command.js');
+
+class Use extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'use',
+      description: 'Uses an item from your inventory.',
+      category: 'Pokèblob',
+      usage: 'use <item number>',
+      guildOnly: true,
+      botPerms: ['SEND_MESSAGES'],
+      permLevel: 'User'
+    });
+  }
+
+  async run(message, args, level) { //eslint-disable-line no-unused-vars
+    const settings = message.settings; // eslint-disable-line no-unused-vars
+    const connection = await this.client.db.acquire();
+    try { // eslint-disable-line no-empty
+
+    } finally {
+      connection.release();
+    }
+  }
+}
+
+module.exports = Use;
