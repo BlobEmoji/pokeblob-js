c91299bb7a8ea2edded84dc1d1f897c2fdc589df
OG Novuh <jamooney2000@gmail.com>
2018-01-14 17:54:37 -0500

    Fix another update error.

--- commands/reboot.js
+++ commands/reboot.js
@@ -18,7 +18,7 @@ class Reboot extends Command {
       this.client.commands.forEach(async cmd => {
         await this.client.unloadCommand(cmd);
       });
-      process.exit(1);
+      process.exit();
     } catch (e) {
       console.log(e);
     }
