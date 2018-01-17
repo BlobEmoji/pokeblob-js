07ad40077a590bf7b815a419ebe663d481dcc32f
Gorialis <sansgorialis@gmail.com>
2018-01-18 03:31:21 +0900

    Make effects effective

--- util/db.js
+++ util/db.js
@@ -163,7 +163,7 @@ class DatabaseBackend {
       VALUES ($1::BIGINT, $2::BIGINT, $3)
       ON CONFLICT (effect_id, user_id)
       DO UPDATE SET
-      life = effects.life + $3,
+      life = effects.life + $3
       RETURNING unique_id, effect_id, user_id, life
     `, [effectID, member.unique_id, life]);
     return res.rows[0];
@@ -177,7 +177,7 @@ class DatabaseBackend {
       FROM effectdefs
       WHERE user_id = $1::BIGINT AND life >= 1
       AND effects.effect_id = effectdefs.unique_id AND effectdefs.type = $2
-      RETURNING effects.unique_id, effects.effect_id, effects.user_id, effects.life
+      RETURNING effects.unique_id, effects.effect_id, effects.user_id, effects.life, effectdefs.name
     `, [member.unique_id, effectType]);
 
     // returns relevant effects
@@ -211,6 +211,18 @@ class DatabaseBackend {
       blobdefs.emoji_id, blobdefs.emoji_name
       FROM blobs
       INNER JOIN blobdefs ON blobs.blob_id = blobdefs.unique_id
+      WHERE user_id = $1::BIGINT
+    `, [member.unique_id]);
+    return res.rows;
+  }
+
+  async getUserEffects(client, guildID, memberID) {
+    const member = await this.ensureMember(client, guildID, memberID);
+    const res = await client.query(`
+      SELECT effects.unique_id, effects.effect_id, effects.life,
+      effectdefs.name
+      FROM effects
+      INNER JOIN effectdefs ON effects.effect_id = effectdefs.unique_id
       WHERE user_id = $1::BIGINT
     `, [member.unique_id]);
     return res.rows;
