9974041f5ef99f9adda524f05deabd9856d6ace2
Gorialis <sansgorialis@gmail.com>
2018-01-19 04:43:58 +0900

    Add blob listing command

---
+++ commands/blobs.js
@@ -0,0 +1,59 @@
+const Command = require('../base/Command.js');
+const { MessageEmbed } = require('discord.js');
+
+class Blobs extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'blobs',
+      description: 'Shows the list of blobs you have, and blobs you\'ve seen.',
+      category: 'Pokéblob',
+      usage: 'blobs',
+      guildOnly: true,
+      botPerms: ['SEND_MESSAGES'],
+      permLevel: 'User'
+    });
+  }
+
+  async run(message, args, level) {
+    const allArgs = args.join(' ').trim();
+    const firstMention = message.mentions.users.first();
+    let parseID;
+    if (!isNaN(allArgs)) parseID = message.guild.member(allArgs);
+    const target = (firstMention) ? firstMention : (parseID) ? parseID.user : message.author;
+
+    const connection = await this.client.db.acquire();
+    let blobData;
+    try {
+      blobData = await this.client.db.getUserBlobs(connection, message.guild.id, target.id);
+    } finally {
+      connection.release();
+    }
+    const blobsOwned = blobData.filter(x => x.caught);
+    const blobsOnHand = blobsOwned.filter(x => x.amount > 0);
+    const blobsOnceOwned = blobsOwned.filter(x => x.amount <= 0);
+    const blobsSeen = blobData.filter(x => !x.caught);
+
+    const onHandFormatting = blobsOnHand.map(x => x.amount > 1 ? `${x.amount}x <:${x.emoji_name}:${x.emoji_id}>` : `<:${x.emoji_name}:${x.emoji_id}>`).join(', ');
+    const onceOwnedFormatting = blobsOnceOwned.map(x => `<:${x.emoji_name}:${x.emoji_id}>`).join(', ');
+    const seenFormatting = blobsSeen.map(x => `<:${x.emoji_name}:${x.emoji_id}>`).join(', ');
+
+    const finalFormatting = `${onHandFormatting === '' ? '' : 'Blobs currently owned:'}`;
+
+    const embed = new MessageEmbed()
+      .setAuthor(target.username, target.displayAvatarURL())
+      .setTimestamp()
+      .setDescription(`This user has seen ${blobData.length} blob(s) in their lifetime.`)
+      .setFooter('PokéBlobs');
+
+    if (onHandFormatting.trim() !== '')
+      embed.addField(`Blobs owned on hand (${blobsOnHand.length})`, onHandFormatting, false);
+    if (onceOwnedFormatting.trim() !== '')
+      embed.addField(`Blobs once owned, but traded away (${blobsOnceOwned.length})`, onceOwnedFormatting, false);
+    if (seenFormatting.trim() !== '')
+      embed.addField(`Blobs seen on adventure (${blobsSeen.length})`, seenFormatting, false);
+
+    message.channel.send({ embed });
+  }
+}
+
+module.exports = Blobs;
